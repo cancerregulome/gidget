e365211f324a416b513bcd3352a3adc366df46f8
smrgit <sheila.reynolds@systemsbiology.org>
2014-02-19 09:12:56 -0800

    added block to handle with "curated" MAF archives from the broad

--- commands/feature_matrix_construction/shscript/wget_maf.sh
+++ commands/feature_matrix_construction/shscript/wget_maf.sh
@@ -36,6 +36,41 @@ for d in `cat $TCGAFMP_ROOT_DIR/shscript/tumor_list.txt`
     done
 
 # =============================================================================
+# broad.mit.edu/illuminaga_dnaseq_curated/mutations/
+for d in `cat $TCGAFMP_ROOT_DIR/shscript/tumor_list.txt`
+
+    do
+
+	echo " "
+	echo " "
+	echo " ******************************************************************** "
+	echo $d
+
+	cd /titan/cancerregulome11/TCGA/repositories/dcc-mirror/public/tumor/$d/
+        mkdir gsc
+        chmod g+w gsc
+        cd gsc
+	mkdir broad.mit.edu
+        chmod g+w broad.mit.edu
+	cd broad.mit.edu
+	mkdir illuminaga_dnaseq_curated
+        chmod g+w illuminaga_dnaseq_curated
+	cd illuminaga_dnaseq_curated
+	mkdir mutations
+        chmod g+w mutations
+	cd mutations
+
+	rm -fr index.html
+	wget -e robots=off --wait 1 --debug --no-clobber --continue --server-response --no-directories \
+	     --accept "*Level_2*.tar.gz" --accept "*mage-tab*.tar.gz" --accept "*CHANGES*txt" \
+             -R "*images*" \
+	     --verbose \
+	     --recursive --level=1 \
+	     https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/$d/gsc/broad.mit.edu/illuminaga_dnaseq_curated/mutations
+
+    done
+
+# =============================================================================
 # genome.wustl.edu/illuminaga_dnaseq/mutations
 for d in `cat $TCGAFMP_ROOT_DIR/shscript/tumor_list.txt`
 
