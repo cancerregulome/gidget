61f9ca60de1963e451e354fa00e30449686ece28
spacepod <ntasman@systemsbiology.org>
2014-03-20 16:37:29 -0700

    replacing checks with standarized utilities.  example for other top-level scripts.

--- commands/feature_matrix_construction/shscript/fmp01B_FH.sh
+++ commands/feature_matrix_construction/shscript/fmp01B_FH.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-: ${LD_LIBRARY_PATH:?" environment variable must be set and non-empty"}
-: ${TCGAFMP_ROOT_DIR:?" environment variable must be set and non-empty"}
-: ${TCGAFMP_DATA_DIR:?" environment variable must be set and non-empty"}
+# every TCGA FMP script should start with these lines:
+: ${TCGAFMP_ROOT_DIR:?" environment variable must be set and non-empty; defines the path to the TCGA FMP scripts directory"}
+source ${TCGAFMP_ROOT_DIR}/tcga_fmp_util.sh
 
-if [[ "$PYTHONPATH" != *"gidget"* ]]; then
-    echo " "
-    echo " your PYTHONPATH should include paths to gidget/commands/... directories "
-    echo " "
-    exit 99
-fi
 
 ## this script should be called with the following parameters:
 ##      date, eg '12jul13' or 'test'
