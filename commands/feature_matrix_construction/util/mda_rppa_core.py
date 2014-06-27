16441a827e12ee9a3b38c6e9491b2df5e3ad590e
Michael Miller <mmiller@systemsbiology.org>
2014-06-27 09:14:55 -0700

    updated RPPA to create features for all genes related to an antibody.  updated test to now go against two branches of gidget, normally master vs. new branch

--- commands/feature_matrix_construction/util/mda_rppa_core.py
+++ commands/feature_matrix_construction/util/mda_rppa_core.py
@@ -19,6 +19,7 @@ class mdanderson_org_mda_rppa_core(technology_type):
         self.configuration.update(config.items('mdanderson_org_mda_rppa_core'))
         self.fname2row = {}
         self.topRowIndex = 0
+        self.gene2ab = {}
 
     def includeFile(self, tokens):
         barcode = tokens[self.iBarcode]
@@ -44,13 +45,15 @@ class mdanderson_org_mda_rppa_core(technology_type):
         ## start out with LOTS of sanity checking of these input values ...
         ## if the first token is actually a list of gene symbols, we will
         ## take only the first one ...
-        tmpTokens = tokens[0].split()
-        fType, fName = self._validateFeatureParts(self.dType, self.fType, tmpTokens[0])
-    
-        ## create the feature name
-        if not self.genename2geneinfo.get(tokens[1]):
-            featureName = self.dType + ":" + fType + ":" + fName + ":::::" + tokens[1]
-            self.genename2geneinfo[tokens[1]] = featureName
+        genes = tokens[0].split()
+        fType, fName = self._validateFeatureParts(self.dType, self.fType, genes[0])
+        
+        if not self.genename2geneinfo.get(tokens[-1]):
+            ## the gene names are the first part of the list, create a feature for each one
+            for gene in genes:
+                featureName = self.dType + ":" + fType + ":" + self._cleanString(gene) + ":::::" + tokens[-1]
+                names = self.genename2geneinfo.setdefault(tokens[-1], [])
+                names += [featureName]
 
     #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
     def postprocess(self, dataMatrix, geneList, sampleList):
@@ -82,18 +85,28 @@ class mdanderson_org_mda_rppa_core(technology_type):
     # do not call _readGeneDetails(), _readDatumDetails() or readNullValue(),  
     # have all the necessary details to decide what row to actually write to
     def _readDataDetails(self, info, tokens, geneList, dataMatrix, sampleIndex):
-        gene = self.genename2geneinfo[tokens[self.tokenGeneIndex]]
-        if not self.fname2row.has_key(gene):
-            geneList += [gene]
-            self.myGeneList += [gene]
-            self.fname2row[gene] = self.topRowIndex
-            self.topRowIndex += 1
-        row = self.fname2row[gene]
-        self.curGeneCount += 1
-        try:
-            dataMatrix[row][sampleIndex] = float(tokens[self.tokenDatumIndex])
-        except Exception as e:
-            if (len(tokens) == 1 or tokens[1] == 'null' or tokens[1] == 'NA'):
-                dataMatrix[row][sampleIndex] = self.NA_VALUE
-            else:
-                raise ValueError('ERROR converting token to float ??? %s: token length: %i tokens: %s', (str(self), len(tokens), tokens), e)
+        ab = tokens[self.tokenGeneIndex]
+        genes = self.genename2geneinfo[ab]
+        for gene in genes:
+            # verify the gene is only associated with one antibody
+            seen_abs = self.gene2ab.setdefault(gene, set())
+            if 0 < len(seen_abs) and ab not in seen_abs:
+                seen_abs.add(ab)
+                raise ValueError("more than one antibody associated with %s: %s" % (gene, seen_abs))
+            seen_abs.add(ab)
+            
+            # add the gene to the feature matrix if we haven't seen it already
+            if not self.fname2row.has_key(gene):
+                geneList += [gene]
+                self.myGeneList += [gene]
+                self.fname2row[gene] = self.topRowIndex
+                self.topRowIndex += 1
+            row = self.fname2row[gene]
+            self.curGeneCount += 1
+            try:
+                dataMatrix[row][sampleIndex] = float(tokens[self.tokenDatumIndex])
+            except Exception as e:
+                if (len(tokens) == 1 or tokens[1] == 'null' or tokens[1] == 'NA'):
+                    dataMatrix[row][sampleIndex] = self.NA_VALUE
+                else:
+                    raise ValueError('ERROR converting token to float ??? %s: token length: %i tokens: %s', (str(self), len(tokens), tokens), e)
