c5ce813a6892b3f9b5302db294c052c9b1d6de7a
Natalie Tasman <ntasman@systemsbiology.org>
2014-09-01 19:22:55 -0700

    version update

--- gidget/gidget.py
+++ gidget/gidget.py
@@ -88,7 +88,7 @@ if __name__ == '__main__':
 
     # for "-h" or "--help", docopt prints usage and exits cleanly
     mainArgs = docopt(__doc__,
-                      version = 'gidget version 0.4.2',
+                      version = 'gidget version 1.0.0-develop',
                       options_first = True)
 
 
