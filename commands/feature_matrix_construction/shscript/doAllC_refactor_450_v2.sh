8b4ec64a18300c533f3a83324ae0b309deca2415
smrgit <sheila.reynolds@systemsbiology.org>
2014-10-11 12:58:30 -0700

    added new option to ignore the TCGA annotations manager

--- commands/feature_matrix_construction/shscript/doAllC_refactor_450_v2.sh
+++ commands/feature_matrix_construction/shscript/doAllC_refactor_450_v2.sh
@@ -12,12 +12,14 @@ source ${TCGAFMP_ROOT_DIR}/../../gidget/util/gidget_util.sh
 ##      a config file, eg 'parse_tcga.config', relative to $TCGAFMP_ROOT_DIR/config
 
 WRONGARGS=1
-if [[ $# != 5 ]] && [[ $# != 6 ]]
+if [[ $# != 5 ]] && [[ $# != 7 ]]
     then
-        echo " Usage   : `basename $0` <curDate> <snapshotName> <tumorType> <config> <public/private> [auxName]"
-        echo " Example : `basename $0` 28oct13  dcc-snapshot-28oct13  brca  parse_tcga.all450k.config  private  aux"
+        echo " Usage   : `basename $0` <curDate> <snapshotName> <tumorType> <config> <public/private> [auxName] [ignoreAM=yes/no]"
+        echo " Example : `basename $0` 28oct13  dcc-snapshot-28oct13  brca  parse_tcga.all450k.config  private  aux  no"
         echo " "
         echo " Note that the new auxName option at the end is optional and will default to simply aux "
+        echo " Note that the ignoreAM (ignore annotations-manager) option is also optional and will default to no "
+        echo " However, if you want to use either of these two optional command-line arguments, you MUST use BOTH "
         exit $WRONGARGS
 fi
 
@@ -27,11 +29,13 @@ oneTumor=$3
 config=$4
 ppString=$5
 
-if (( $# == 6 ))
+if (( $# == 7 ))
     then
         auxName=$6
+        ignoreAM=$7
     else
         auxName=aux
+        ignoreAM=no
 fi
 
 if [ ! -d $TCGAFMP_DATA_DIR/$oneTumor ]
@@ -50,7 +54,7 @@ rm -fr $TCGAFMP_DATA_DIR/$oneTumor/scratch/fmp*.$curDate.$oneTumor.log
 rm -fr $TCGAFMP_DATA_DIR/$oneTumor/$curDate/*.*
 
 
-$TCGAFMP_ROOT_DIR/shscript/fmp01B_xml_refactor.sh $curDate $snapshotName $oneTumor $config $ppString $auxName >& $TCGAFMP_DATA_DIR/$oneTumor/scratch/fmp01B.$curDate.$snapshotName.$oneTumor.log
+$TCGAFMP_ROOT_DIR/shscript/fmp01B_xml_refactor.sh $curDate $snapshotName $oneTumor $config $ppString $auxName $ignoreAM >& $TCGAFMP_DATA_DIR/$oneTumor/scratch/fmp01B.$curDate.$snapshotName.$oneTumor.log
 $TCGAFMP_ROOT_DIR/shscript/fmp02B_L3_refactor_450.sh  $curDate $snapshotName $oneTumor $config >& $TCGAFMP_DATA_DIR/$oneTumor/scratch/fmp02B.$curDate.$snapshotName.$oneTumor.log
 
 $TCGAFMP_ROOT_DIR/shscript/fmp05B_filter_450.sh $curDate $oneTumor $auxName >& $TCGAFMP_DATA_DIR/$oneTumor/scratch/fmp05B.$curDate.$oneTumor.log
