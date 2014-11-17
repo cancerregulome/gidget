16502b2ac6e07100b9ed02a87317f2e3da26c244
smrgit <sheila.reynolds@systemsbiology.org>
2014-11-17 05:58:21 -0800

    new script to grab the "latestarchive" file from the DCC and put it in $TCGAFMP_DCC_REPOSITORIES/dcc-mirror/datareports/resources

---
+++ commands/feature_matrix_construction/shscript/wget_latestArchive.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# every TCGA FMP script should start with these lines:
+: ${TCGAFMP_ROOT_DIR:?" environment variable must be set and non-empty; defines the path to the TCGA FMP scripts directory"}
+source ${TCGAFMP_ROOT_DIR}/../../gidget/util/gidget_util.sh
+
+
+cd $TCGAFMP_DCC_REPOSITORIES/dcc-mirror/datareports/resources
+
+rm -fr latestarchive
+wget http://tcga-data.nci.nih.gov/datareports/resources/latestarchive
+
+wc -l latestarchive
+
+rm -fr t?
+rm -fr tumor_archive_counts.Level_?.txt
+rm -fr platform_archive_counts.Level_?.txt
+
+cut -f1 latestarchive | sed -e '1,$s/_/	/' | cut -f2 | sed -e '1,$s/\./	/g' >& t0
+
+
+grep "Level_1" t0 >& t1
+grep "Level_2" t0 >& t2
+grep "Level_3" t0 >& t3
+grep "mage-tab" t0 >& tm
+
+cut -f1 t1 | sort | uniq -c >& tumor_archive_counts.Level_1.txt
+cut -f1 t2 | sort | uniq -c >& tumor_archive_counts.Level_2.txt
+cut -f1 t3 | sort | uniq -c >& tumor_archive_counts.Level_3.txt
+
+cut -f2 t1 | sort | uniq -c >& platform_archive_counts.Level_1.txt
+cut -f2 t2 | sort | uniq -c >& platform_archive_counts.Level_2.txt
+cut -f2 t3 | sort | uniq -c >& platform_archive_counts.Level_3.txt
+
+rm -fr t?
