d411e7289ad434a26768dff1b2a0308f255f3e1c
Nicholas Wilson <nwilson@systemsbiology.org>
2015-02-13 13:54:19 -0800

    WIP full pipe

--- gidget/gidget_run_all.py
+++ gidget/gidget_run_all.py
@@ -2,7 +2,7 @@
 
 """Gidget Run All
 
-usage: gidget_run_all [options] <maf_manifest>
+usage: gidget_run_all [options] <maf_manifest> <output_directory>
 
 Options:
     -h, --help       Show this screen
@@ -14,7 +14,10 @@ Options:
 from docopt import docopt
 from threading import Semaphore, Thread
 from subprocess import Popen
+from datetime import datetime
+from fnmatch import fnmatch
 import csv
+import os
 
 # tsv parser settings
 MAF_MANIFEST_DIALECT = "maf_manifest"
@@ -29,41 +32,94 @@ PATH       = 'internal-path'
 
 _processSemaphore = None
 
+class PipelineError(Exception):
+    pass
 
 class Pipeline(Thread):
 
-    def __init__(self, mafFile):
-        self.maf = mafFile
+    def __init__(self, mafFile, outputDirRoot, tagString, tumorString):
+        self.maf = os.path.expandvars(mafFile)
+        self.tumorString = tumorString
+
+        self.outputDir = os.path.join(outputDirRoot, tagString)
+
+        _ensureDir(self.outputDir)
+        tumorDir = os.path.join(self.outputDir, self.tumorString)
+        _ensureDir(tumorDir)
+
+        self.dateString = datetime.now().strftime('%Y_%m_%d')
+        self.dateDir = os.path.join(tumorDir, self.dateString)
+
+        if os.path.exists(self.dateDir):
+            self.dateString = datetime.now().strftime('%Y_%m_%d-%H:%M')
+            self.dateDir = os.path.join(tumorDir, self.dateString)
+
+        _ensureDir(self.dateDir)
+
+        self.env = os.environ.copy()
+        self.env["TCGAFMP_DATA_DIR"] = self.outputDir
+
         super(Pipeline, self).__init__()
 
     def run(self):
         with _processSemaphore:
-            # TODO
-            sub = Popen(("echo", self.maf))
-            sub.wait()
+            self.executeGidgetPipeline('annotate-maf.sh', (self.maf, ))
+
+            # this is just where the annotation and binarization scripts put things. Don't ask too many questions...
+            outputdir = os.path.join(self.dateDir, self.tumorString)
+            annotationOutput = os.path.join(outputdir, os.path.basename(self.maf) + '.ncm.with_uniprot')
+
+            self.executeGidgetPipeline('binarization.sh', (self.tumorString, annotationOutput))
+
+            binarizationOutput = None
+            for outfile in os.listdir(os.curdir):
+                if fnmatch(outfile, 'mut_bin_*.txt'):
+                    binarizationOutput = outfile
+                    break
+            if (binarizationOutput is None):
+                raise PipelineError()  # TODO do something useful
+
+            self.executeGidgetPipeline('post_maf.sh', (self.tumorString, binarizationOutput))
+
+            ppstring = 'private'  # TODO is it always this way?
+
+            self.executeGidgetPipeline('fmx.sh', (self.dateString, self.tumorString, ppstring))
+
+            # TODO load into re
 
 
-def run_all(pathToMafManifest, numProcesses):
+    def executeGidgetPipeline(self, pipeline, args):
+        subProc = Popen((os.path.join(os.environ['GIDGET_SOURCE_ROOT'], 'pipelines', pipeline),) + args, cwd=self.dateDir)
+        if subProc.wait() != 0:
+            # TODO log error message
+            raise PipelineError()  # TODO just exit?
+
+def run_all(pathToMafManifest, numProcesses, outputDir):
     global _processSemaphore
-    _processSemaphore = Semaphore(numProcesses)
+    _processSemaphore = Semaphore(numProcesses)  # TODO there is probably a better way than using a global semaphore. Or is there...?
     with open(pathToMafManifest) as tsv:
         for maf in csv.DictReader(tsv, dialect=MAF_MANIFEST_DIALECT):
-            run_one(maf[PATH])
+            print maf
+            run_one(maf[PATH], outputDir, maf[TAGS], maf[TUMOR_CODE])
 
 
-def run_one(pathToMaf):
+def run_one(pathToMaf, outputDir, tags, tumorString):
     """
     :param pathToMaf:
     :return the pipeline thread running this maf file
     """
     assert _processSemaphore is not None
-    pipeline = Pipeline(pathToMaf)
+    pipeline = Pipeline(pathToMaf, outputDir, tags, tumorString)
     pipeline.start()
     return pipeline
 
 
+def _ensureDir(absPath):
+    if not os.path.exists(absPath):
+        os.mkdir(absPath)
+
 if __name__ == "__main__":
     arguments = docopt(__doc__, version='Gidget Run All 0.0')
     print(arguments)
-    run_all(arguments.get('<maf_manifest>'), arguments.get('--processes') or 4)
+    run_all(arguments.get('<maf_manifest>'), arguments.get('--processes') or 4, arguments.get('<output_directory>'))
 
