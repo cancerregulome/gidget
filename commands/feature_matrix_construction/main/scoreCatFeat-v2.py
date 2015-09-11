cf28074e2270adfec6c8e8b21e9d1b2481a80985
smrgit <sheila.reynolds@systemsbiology.org>
2015-09-11 08:29:17 -0700

    switched to latest pairwise binary: pairwise-3.0.1rc1-rel

--- commands/feature_matrix_construction/main/scoreCatFeat-v2.py
+++ commands/feature_matrix_construction/main/scoreCatFeat-v2.py
@@ -459,7 +459,7 @@ if __name__ == "__main__":
         listFile = tmpDir + "/%d.list" % eachI
 
         ## cmdString = "1 ignoreThree.py " + gidgetConfigVars['TCGAFMP_PAIRWISE_ROOT'] + "/pairwise-2.1.2"
-        cmdString = "1 ignoreThree.py " + gidgetConfigVars['TCGAFMP_PAIRWISE_ROOT'] + "/pairwise-3.0.0rc1-rel"
+        cmdString = "1 ignoreThree.py " + gidgetConfigVars['TCGAFMP_PAIRWISE_ROOT'] + "/pairwise-3.0.1rc1-rel"
         cmdString += " --by-index %s " % listFile
         ## cmdString += " --dry-run "
         cmdString += " --p-value 2. "
@@ -485,7 +485,7 @@ if __name__ == "__main__":
     #     runlist /<path-to-scratch-space>/runList.txt
     cmdString = "python %s/main/golem.py " % gidgetConfigVars['TCGAFMP_ROOT_DIR']
     cmdString += "http://glados.systemsbiology.net:7083 -p " + golempwd + " "
-    cmdString += "-L pairwise-3.0.0 -u "
+    cmdString += "-L pairwise-3.0.1rc1-rel -u "
     cmdString += getpass.getuser() + " "
     cmdString += "runlist " + runFile
     print cmdString
